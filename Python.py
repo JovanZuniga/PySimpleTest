ef95f4d76fcb63fcc86d901cc6afd34a8244877e
Jovan <jovartzy@gmail.com>
2022-12-20 11:48:46 -0500

    Creates basic window

--- Python.py
+++ Python.py
@@ -1,1 +1,11 @@
-ECHO is on.
+import PySimpleGUI as sg
+
+layout = [[sg.Text('Test')]]
+window = sg.Window('Window Title', layout)
+
+while True: 
+    event, values = window.read()
+    if event == sg.WIN_CLOSED:
+        break
+
+window.close()
